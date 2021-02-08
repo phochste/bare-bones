c4a7516062cda2b19accdbdee356a04311068489
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2021-02-08 08:33:19 +0100

    Adding following route

--- activitypub/lib/appserver.pm
+++ activitypub/lib/appserver.pm
@@ -12,10 +12,6 @@ our $PRIVATE_PEM      = Catmandu->config->{keys}->{private};
 our $PUBLIC_PEM       = Catmandu->config->{keys}->{public};
 our $PAGE_SIZE        = Catmandu->config->{page_size};
 
-sub followers {
-    Catmandu->store("followers")->bag;
-}
-
 # Webfinger is how we are going to learn where we find information about
 # MYSELF@my-eexample.com
 get '/.well-known/webfinger' => sub {
@@ -108,8 +104,8 @@ post '/actor/:name/inbox' => sub {
 };
 
 # Show a list of followers...
-get '/actor/:name/followers' => sub {
-    my $actor = param('name');
+get qr{/actor/(\w+)/(followers|following)} => sub {
+    my ($actor,$follower_or_following) = splat;
     my $page  = params->{page};
 
     # We only know MYSELF
@@ -118,37 +114,43 @@ get '/actor/:name/followers' => sub {
         return 'No such user';
     }
 
-    my $count = followers->count;
+    my $bag   = Catmandu->store($follower_or_following)->bag;
+    my $count = $bag->count;
 
     my $response = {
        "\@context"   => "https://www.w3.org/ns/activitystreams",
        "totalItems"  => $count,
-       "id"          => "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/followers",
+       "id"          => "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/$follower_or_following",
        "type"        => "OrderedCollection",
     };
 
-    if ($page =~ /^\d+/ && $page > 0) {
+    if ($page && $page =~ /^\d+/) {
         my $items   = [];
 
-        followers->slice($page - 1 , $PAGE_SIZE)->each(sub {
+        $bag->slice($page * $PAGE_SIZE, $PAGE_SIZE)->each(sub {
             push @$items , $_[0]->{_id};
         });
 
         $response->{"id"}          .= "?page=$page";
         $response->{"type"}         = "OrderedCollectionPage";
         $response->{"orderedItems"} = $items;
-        $response->{"partOf"}       = "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/followers";
+        $response->{"partOf"}       = "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/$follower_or_following";
 
-        if ($page * $PAGE_SIZE < $count ) {
-            $response->{"next"} = "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/followers?page=" . ($page+1);
+        if (($page + 1) * $PAGE_SIZE < $count ) {
+            $response->{"next"} = "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/$follower_or_following?page=" . ($page+1);
+        }
+        else {
+            # we are on the last page...
         }
     }
     else {
         $response->{"type"}  = "OrderedCollection";
-    }
-
-    if ($count) {
-        $response->{"first"} = "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME?page=1";
+        if ($count) {
+            $response->{"first"} = "https://$BASE_DOMAIN/actor/$MYSELF_PREF_NAME/$follower_or_following?page=1";
+        }
+        else {
+            # no followers...
+        }
     }
 
     # Return an ActivityStream document listing the followers
